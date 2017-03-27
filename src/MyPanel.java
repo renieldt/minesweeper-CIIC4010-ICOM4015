516d04c98ffacf1e661628930418b6dd5509d66b
naxsor <lemanuel.colon@upr.edu>
2017-03-27 14:55:44 -0400

    -Undo merge, added checkField, placeFlag, isMine methods.

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,5 +1,4 @@
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Insets;
 import java.util.Random;
@@ -13,35 +12,19 @@ public class MyPanel extends JPanel {
 	private static final int INNER_CELL_SIZE = 29;
 	private static final int TOTAL_COLUMNS = 9;
 	private static final int TOTAL_ROWS = 10;   //Last row has only one cell
-	public int minesWanted = 15; //maybe 20 son muchas
+	private int minesWanted = 15;
 	public Color[][] mineField = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
-	public boolean minesOnField[][] = new boolean [TOTAL_COLUMNS][TOTAL_ROWS];
-	public int [][] minas = new int [TOTAL_COLUMNS][TOTAL_ROWS];
+	public int[][] minas = new int [TOTAL_COLUMNS][TOTAL_ROWS];
 	public String [][] gridAmount = new String [TOTAL_COLUMNS][TOTAL_ROWS];
+	public boolean minesOnField[][] = new boolean [TOTAL_COLUMNS][TOTAL_ROWS];
 	public int x = -1;
 	public int y = -1;
-	public int mouseDownGridX = 0; 
+	public int mouseDownGridX = 0;
 	public int mouseDownGridY = 0;
 	public int difficult = 0;
 	
+	
 	public MyPanel() {   //This is the constructor... this code runs first to initialize
-		
-		//Generates mines. If a square or tile is already a mine, generates a mine in a different tile
-		for (int i = 0; i < minesWanted; i++){
-			for (int j = 0; j < 1; j++){
-				Random generator = new Random();
-				int x = generator.nextInt(TOTAL_COLUMNS);
-				int y = generator.nextInt(TOTAL_ROWS);
-
-				if(minesOnField[x][y] == false){
-					minesOnField[x][y] = true;
-				}
-				else {
-					i--;
-				}
-			}
-		}
-		
 		if (INNER_CELL_SIZE + (new Random()).nextInt(1) < 1) {	//Use of "random" to prevent unwanted Eclipse warning
 			throw new RuntimeException("INNER_CELL_SIZE must be positive!");
 		}
@@ -80,34 +63,41 @@ public class MyPanel extends JPanel {
 		int height = y2 - y1;
 
 		//Paint the background
-		g.setColor(Color.LIGHT_GRAY);
-		g.fillRect(x1, y1, width + 1, height + 1);
+		g.setColor(Color.PINK);
+		g.fillRect(x1, y1, width + 1, height+1);
 
 		//Draw the grid minus the bottom row (which has only one cell)
 		//By default, the grid will be 10x10 (see above: TOTAL_COLUMNS and TOTAL_ROWS) 
 		g.setColor(Color.BLACK);
-		for (int y = 0; y <= TOTAL_ROWS; y++) {
+		for (int y = 0; y <= TOTAL_ROWS - 1; y++) {
 			g.drawLine(x1 + GRID_X, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)), x1 + GRID_X + ((INNER_CELL_SIZE + 1) * TOTAL_COLUMNS), y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)));
 		}
-		for (int x = 0; x < TOTAL_COLUMNS + 1; x++) {
+		for (int x = 0; x <= TOTAL_COLUMNS; x++) {
 			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)));
 		}
+
+		//Draw an additional cell at the bottom left
+		//g.drawRect(x1 + GRID_X, y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)), INNER_CELL_SIZE + 1, INNER_CELL_SIZE + 1);
+
 		//Paint cell colors
 		for (int x = 0; x < TOTAL_COLUMNS; x++) {
-			for (int y = 0; y < TOTAL_ROWS; y++) {
-				if ((x == 0) || (y != TOTAL_ROWS)) {
+			for (int y = 0; y < TOTAL_ROWS - 1; y++) {
+				if ((x == 0) || (y != TOTAL_ROWS - 1)) {
 					Color c = mineField[x][y];
 					g.setColor(c);
-					g.fillRect(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 1, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 1, INNER_CELL_SIZE, INNER_CELL_SIZE);
-					Color green = new Color (0, 153, 0);
-					g.setColor(green);
-					g.setFont(new Font("American Typewriter", Font.PLAIN, 14));
-					g.drawString(gridAmount[x][y], x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 10, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 20);
-
+					g.fillRect(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 1, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1))+1, INNER_CELL_SIZE, INNER_CELL_SIZE);
 				}
 			}
-		}
+		}	
 	}
+	
+	public int getColumns(){
+		return TOTAL_COLUMNS;
+	}
+	public int getROWS(){
+		return TOTAL_ROWS;
+	}
+	
 	public int getGridX(int x, int y) {
 		Insets myInsets = getInsets();
 		int x1 = myInsets.left;
@@ -125,15 +115,16 @@ public class MyPanel extends JPanel {
 		}
 		x = x / (INNER_CELL_SIZE + 1);
 		y = y / (INNER_CELL_SIZE + 1);
-		if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
+		/*if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
 			return x;
-		}
+		}*/
 		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
 			return -1;
 		}
 		return x;
 	}
 	public int getGridY(int x, int y) {
+
 		Insets myInsets = getInsets();
 		int x1 = myInsets.left;
 		int y1 = myInsets.top;
@@ -150,24 +141,22 @@ public class MyPanel extends JPanel {
 		}
 		x = x / (INNER_CELL_SIZE + 1);
 		y = y / (INNER_CELL_SIZE + 1);
-		if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
+		/*if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
 			return y;
-		}
+		}*/
 		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
 			return -1;
 		}
 		return y;
 	}
 	
-	
-	// To determine if a tile contains a mine (Reniel)
-		public boolean isMine(int x, int y) {
-			if (minesOnField[x][y] == true){
-				return true;
-			}
-			return false;
+	public boolean isMine(int x, int y) {
+		if (minas[x][y] == -1){
+			return true;
 		}
-	
+		return false;
+}
+			
 	//By Lemanuel Colon
 	public void genMines(){
 				int minesPlaced = 0;
@@ -186,20 +175,51 @@ public class MyPanel extends JPanel {
 	//By Lemanuel Colon
 	public void readMines(int x, int y){
 		if(minas[x][y] == -1){
-			Color newColor = Color.BLACK;
-			mineField[x][y] = newColor;
+			mineField[x][y] = Color.BLACK;
 			this.repaint();
 		} else {
-			Color newColor = Color.LIGHT_GRAY;
-			mineField[x][y] = newColor;
-			this.repaint();
-			
+			minas[x][y] = 0; 
+			mineField[x][y] = Color.LIGHT_GRAY;
+			this.checkField(x, y);
+			this.repaint();	
 		}
-		
 	}
+	
+	
+	public void placeFlag(int x, int y){
+			mineField[x][y] = Color.RED;
+			this.repaint();
+	}
+	
+	public void checkField(int x, int y){
+		if(minas[x][y] == 0){
+			if(minas[x-1][y-1] == 0){
+				mineField[x-1][y-1] = Color.LIGHT_GRAY;
+			}
+			if(minas[x-1][y] == 0){
+				mineField[x-1][y] = Color.LIGHT_GRAY;
+			}
+			if(minas[x-1][y+1] == 0){
+				mineField[x-1][y+1] = Color.LIGHT_GRAY;
+			}
+			if(minas[x][y+1] == 0){
+				mineField[x][y+1] = Color.LIGHT_GRAY;
+			}
+			if(minas[x+1][y+1] == 0){
+				mineField[x+1][y+1] = Color.LIGHT_GRAY;
+			}
+			if(minas[x+1][y] == 0){
+				mineField[x+1][y] = Color.LIGHT_GRAY;
+			}
+			if(minas[x+1][y-1] == 0){
+				mineField[x+1][y-1] = Color.LIGHT_GRAY;
+			}
+			if(minas[x][y+1] == 0){
+				mineField[x][y+1] = Color.LIGHT_GRAY;
+			}
+		}	
+	}
+	
+	
 }
-
 	
-
-	
-
